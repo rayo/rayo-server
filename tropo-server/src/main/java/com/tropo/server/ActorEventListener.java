0a54c52c01096807c45f9734fa83ab369d4d228a
loopingrage <jose@voxeo.com>
2011-06-03 17:28:03 -0400

    Component events are now always dispatched on an actor thread [fixes #36]

---
+++ tropo-server/src/main/java/com/tropo/server/ActorEventListener.java
@@ -0,0 +1,18 @@
+package com.tropo.server;
+
+import com.voxeo.moho.event.EventSource;
+import com.voxeo.moho.utils.Event;
+import com.voxeo.moho.utils.EventListener;
+
+public class ActorEventListener implements EventListener<Event<EventSource>> {
+
+    private Actor actor;
+
+    public ActorEventListener(Actor actor) {
+        this.actor = actor;
+    }
+
+    public void onEvent(Event<EventSource> event) throws Exception {
+        actor.publish(event);
+    }
+}
