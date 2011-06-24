6a3cd034fc7efc5285dfa5456c6a73aebbcfc231
mpermar <mpermar@gmail.com>
2011-06-24 13:22:41 +0200

    Reverted back to do not extend complete event.

--- tropo-core/src/main/java/com/tropo/core/verb/SayCompleteEvent.java
+++ tropo-core/src/main/java/com/tropo/core/verb/SayCompleteEvent.java
@@ -1,5 +1,27 @@
 package com.tropo.core.verb;
 
-public class SayCompleteEvent extends OutputCompleteEvent {
+public class SayCompleteEvent extends VerbCompleteEvent {
+
+    public enum Reason implements VerbCompleteReason {
+        SUCCESS
+    }
+
+    public SayCompleteEvent() {}
+    
+    public SayCompleteEvent(Verb verb) {
+        super(verb);
+    }
+
+    public SayCompleteEvent(VerbCompleteReason reason) {
+        super(reason);
+    }
+
+    public SayCompleteEvent(Output verb, VerbCompleteReason reason) {
+        super(verb, reason);
+    }
+
+    public SayCompleteEvent(Output verb, String errorText) {
+        super(verb, errorText);
+    }
 
 }
