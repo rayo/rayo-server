6ed11f346ae86c31e062a7e20a2d91bb9a1eb33a
mpermar <mpermar@gmail.com>
2011-08-03 16:35:27 +0200

    Fixed issue with SSML validation. SSML schema was dependant on XML standard schema and was downloading it from w33.org which took a while and sometimes timed out. Now xml.xsd is a local file and we load it locally.
    This closes #128

--- tropo-server/src/main/java/com/tropo/server/validation/SsmlValidator.java
+++ tropo-server/src/main/java/com/tropo/server/validation/SsmlValidator.java
@@ -1,6 +1,5 @@
 package com.tropo.server.validation;
 
-import java.io.InputStream;
 import java.io.StringReader;
 
 import javax.annotation.PostConstruct;
@@ -10,84 +9,40 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
-import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.Resource;
 
 import com.tropo.core.validation.ValidationException;
 import com.voxeo.logging.Loggerf;
 
 public class SsmlValidator {
 
-	private static final Loggerf logger = Loggerf.getLogger(SsmlValidator.class);
-	private static int TIMEOUT = 10000;
-	
+	private static final Loggerf logger = Loggerf.getLogger(SsmlValidator.class);	
 	private Schema schema;
-	private Thread initThread;
+	Resource resource;
 
 	@PostConstruct
 	public void init() {
 
-		final SchemaFactory sf = SchemaFactory
-				.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-		logger.debug("Using schema factory", sf);
-		InputStream is = Thread.currentThread().getContextClassLoader()
-				.getResourceAsStream("synthesis-core.xsd");
-		if (is == null) {
-			logger.error("Could not find the SSML Schema in the classpath. SSML validation will be turned off");
-			return;
-		}
-		try {
-			final String strSchema = IOUtils.toString(is);
+		final SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 
-			// XML compilation is a very slow operation. 
-			initThread = new Thread(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						schema = sf.newSchema(new StreamSource(new StringReader(strSchema)));
-					} catch (Exception e) {
-						logger.error("Could not initialize SSML Validation. SSML Validation will be turned off");
-						e.printStackTrace();
-					}					
-				}
-			});
-			initThread.start();
+		try {
+			schema = sf.newSchema(resource.getFile());
 		} catch (Exception e) {
-			logger.error("Could not initialize SSML Validation. SSML Validaiton will be turned off");
+			logger.error("Could not initialize SSML Validation. SSML Validation will be turned off");
 			e.printStackTrace();
 		}
 	}
-	
-	void waitToInitialize(long timeout) {
-		
-		try {
-			initThread.join(timeout);
-		} catch (InterruptedException e) {
-			throw new IllegalStateException(e.getMessage());
-		}
-	}
-	
 	public void validateSsml(String ssml) throws ValidationException {
-		
-		if (schema == null) {
-			if (initThread.isAlive()) {
-				try {
-					initThread.join(TIMEOUT);
-				} catch (InterruptedException e) {
-					if (initThread.isAlive()) {
-						logger.debug("Could not validate SSML. Schema validation is off");
-					}
-				}
-			} else {
-				logger.debug("Could not validate SSML. Schema validation is off");
-				return;
-			}
-		}
-		
+				
 		Validator validator = schema.newValidator();
 		try {
 			validator.validate(new StreamSource( new StringReader(ssml)));
 		} catch (Exception e) {
 			throw new ValidationException("Invalid SSML: " + e.getMessage());
 		}
+	}
+	
+	public void setResource(Resource resource) {
+		this.resource = resource;
 	}
 }
