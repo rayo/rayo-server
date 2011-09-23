c63761cad5881e182d1ec7009d824dfbfea77b5d
mpermar <mpermar@gmail.com>
2011-09-23 08:10:18 +0200

    Supress warnings for serial

--- rayo-server/src/main/java/com/rayo/server/SpringXmppServlet.java
+++ rayo-server/src/main/java/com/rayo/server/SpringXmppServlet.java
@@ -14,6 +14,7 @@ import org.springframework.web.context.support.XmlWebApplicationContext;
 import com.voxeo.logging.Loggerf;
 import com.voxeo.servlet.xmpp.XmppServlet;
 
+@SuppressWarnings("serial")
 public class SpringXmppServlet extends XmppServlet {
 
     private Loggerf log = Loggerf.getLogger(SpringXmppServlet.class);
