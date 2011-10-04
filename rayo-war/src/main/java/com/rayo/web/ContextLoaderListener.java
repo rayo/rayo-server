9c5071cd52c02ee9dfc8a7dc14eb986323d4b9ad
mpermar <mpermar@gmail.com>
2011-10-04 16:11:08 +0200

    cleanup imports

--- rayo-war/src/main/java/com/rayo/web/ContextLoaderListener.java
+++ rayo-war/src/main/java/com/rayo/web/ContextLoaderListener.java
@@ -8,7 +8,6 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.rayo.server.AdminService;
 import com.rayo.server.jmx.Info;
-import com.rayo.server.servlet.RayoServlet;
 
 public class ContextLoaderListener extends org.springframework.web.context.ContextLoaderListener {
 
