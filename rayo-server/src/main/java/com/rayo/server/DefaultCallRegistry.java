e85e965b91fd011df221fb30118c90d27a8f5d8e
mpermar <mpermar@gmail.com>
2011-10-11 17:22:15 +0200

    Added some log statements.

--- rayo-server/src/main/java/com/rayo/server/DefaultCallRegistry.java
+++ rayo-server/src/main/java/com/rayo/server/DefaultCallRegistry.java
@@ -5,25 +5,40 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import com.voxeo.logging.Loggerf;
+
 public class DefaultCallRegistry implements CallRegistry {
 
+	private static Loggerf log = Loggerf.getLogger(DefaultCallRegistry.class);
     private Map<String, CallActor<?>> calls = new ConcurrentHashMap<String, CallActor<?>>();
 
     private JIDRegistry jidRegistry;
     
     @Override
     public void add(CallActor<?> actor) {
+    	
+    	if (log.isDebugEnabled()) {
+    		log.debug("Adding call [%s] to registry: [%s]", actor.getCall().getId(), this);
+    	}
         calls.put(actor.getCall().getId(), actor);
     }
 
     @Override
     public void remove(String id) {
-        calls.remove(id);
+
+    	if (log.isDebugEnabled()) {
+    		log.debug("Removing call [%s] from registry [%s]", id, this);
+    	}
+    	calls.remove(id);
         jidRegistry.remove(id);
     }
 
     @Override
     public CallActor<?> get(String id) {
+    	
+    	if (log.isDebugEnabled()) {
+    		log.debug("Looking up call [%s] in registry: [%s]", id, this);
+    	}    	
         return calls.get(id);
     }
     
