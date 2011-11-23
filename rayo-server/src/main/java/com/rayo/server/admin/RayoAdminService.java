5a5590733c9b236f2889c99eb0589b31fd281711
mpermar <mpermar@gmail.com>
2011-11-23 09:21:34 +0100

    some docs

--- rayo-server/src/main/java/com/rayo/server/admin/RayoAdminService.java
+++ rayo-server/src/main/java/com/rayo/server/admin/RayoAdminService.java
@@ -1,39 +1,36 @@
 package com.rayo.server.admin;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-
 import com.rayo.server.CallActor;
 import com.rayo.server.CallRegistry;
-import com.rayo.server.listener.AdminListener;
 import com.voxeo.logging.Loggerf;
 import com.voxeo.moho.Call;
 import com.voxeo.moho.common.event.MohoInputDetectedEvent;
 import com.voxeo.moho.event.InputDetectedEvent;
 
+/**
+ * <p>This class extends {@link AdminService} adding particular administrative 
+ * operations that only affect Rayo Nodes.</p> 
+ * 
+ * @author martin
+ *
+ */
 public class RayoAdminService extends AdminService {
 
 	private static final Loggerf log = Loggerf.getLogger(RayoAdminService.class);
-	private AtomicBoolean quiesceMode = new AtomicBoolean(false);
 		
-	private ReentrantReadWriteLock adminLock = new ReentrantReadWriteLock();
-	
 	private CallRegistry callRegistry;
-	
-	public boolean isQuiesceMode() {
 		
-		Lock lock = adminLock.readLock();
-		lock.lock();
-		try {
-			return quiesceMode.get();
-		} finally {
-			lock.unlock();
-		}
-	}
-	
+	/**
+	 * Sends a DTMF tone to a call
+	 * 
+	 * @param callId Id of the call
+	 * @param dtmf DTMF tone
+	 */
 	public void sendDtmf(String callId, String dtmf) {
 		
+		if (log.isDebugEnabled()) {
+			log.debug("Sending DTMF tone [%s] to call id [%s]", dtmf, callId);
+		}
 		CallActor<?> actor = callRegistry.get(callId);
 		InputDetectedEvent<Call> event = new MohoInputDetectedEvent<Call>(actor.getCall(), dtmf);
 		try {
@@ -43,42 +40,12 @@ public class RayoAdminService extends AdminService {
 			e.printStackTrace();
 		}
 	}
-
-	public void disableQuiesce() {
-		
-		Lock lock = adminLock.writeLock();
-		lock.lock();
-		try {
-			log.debug("Quiesce Mode has been DISABLED");
-			quiesceMode.set(false);
-			for (AdminListener listener: getAdminListeners()) {
-				listener.onQuiesceModeExited();
-			}
-		} finally {
-			lock.unlock();
-		}
-	}
 	
-	public void enableQuiesce() {
-
-		Lock lock = adminLock.writeLock();
-		lock.lock();
-		try {
-			log.debug("Quiesce Mode has been ENABLED");
-			quiesceMode.set(true);
-			for (AdminListener listener: getAdminListeners()) {
-				listener.onQuiesceModeEntered();
-			}
-		} finally {
-			lock.unlock();
-		}
-	}
-	
-	public boolean getQuiesceMode() {
-		
-		return isQuiesceMode();
-	}
-	
+	/**
+	 * Sets the call registry
+	 * 
+	 * @param callRegistry Call registry
+	 */
 	public void setCallRegistry(CallRegistry callRegistry) {
 		this.callRegistry = callRegistry;
 	}
