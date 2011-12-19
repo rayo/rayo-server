697791216decb541cad0851cd5c0a73fef75b7fc
mpermar <mpermar@gmail.com>
2011-12-19 16:10:26 +0100

    New tests for message filter return values and exceptions.

--- rayo-server/src/test/groovy/com/rayo/server/filter/FilterChainTest.groovy
+++ rayo-server/src/test/groovy/com/rayo/server/filter/FilterChainTest.groovy
@@ -9,11 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 
+import com.rayo.server.exception.RayoProtocolException;
 import com.rayo.server.filter.FilterChain;
 import com.rayo.server.filter.MessageFilter;
 import com.rayo.core.CallCommand;
 import com.rayo.core.CallEvent;
 import com.rayo.core.validation.ValidationException;
+import com.voxeo.servlet.xmpp.StanzaError.Condition;
+import com.voxeo.servlet.xmpp.StanzaError.Type;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations=["/rayo-context-activemq-cdr.xml"])
@@ -87,6 +90,43 @@ public class FilterChainTest {
 	}
 	
 	@Test
+	public void testNullCommandInterruptsFilterChain() throws InterruptedException {
+		
+		def queue = new LinkedList()
+		def filter1 = [handleCommandRequest:{command, chain -> queue.offer("filter1"); return command}] as MessageFilter
+		def filter2 = [handleCommandRequest:{command, chain -> null}] as MessageFilter
+		def filter3 = [handleCommandRequest:{command, chain -> queue.offer("filter3"); return command}] as MessageFilter
+		filtersChain.addFilter(filter1)
+		filtersChain.addFilter(filter2)
+		filtersChain.addFilter(filter3)
+		
+		def callCommand = [:] as CallCommand
+		filtersChain.handleCommandRequest(callCommand)
+		assertEquals queue.poll(),"filter1"
+		assertNull queue.poll()
+	}
+	
+	@Test
+	public void testProtocolExceptionInCommandInterruptsFilterChain() throws InterruptedException {
+		
+		def queue = new LinkedList()
+		def filter1 = [handleCommandRequest:{command, chain -> queue.offer("filter1"); return command}] as MessageFilter
+		def filter2 = [handleCommandRequest:{command, chain -> throw new RayoProtocolException(Condition.BAD_REQUEST,Type.CANCEL,"error")}] as MessageFilter
+		def filter3 = [handleCommandRequest:{command, chain -> queue.offer("filter3"); return command}] as MessageFilter
+		filtersChain.addFilter(filter1)
+		filtersChain.addFilter(filter2)
+		filtersChain.addFilter(filter3)
+		
+		try {
+			def callCommand = [:] as CallCommand
+			filtersChain.handleCommandRequest(callCommand)
+			fail("Expected Exception")
+		} catch (RayoProtocolException e) {}
+		assertEquals queue.poll(),"filter1"
+		assertNull queue.poll()
+	}
+	
+	@Test
 	public void testCommandResponseExecutedInSequence() throws InterruptedException {
 		
 		def queue = new LinkedList()
@@ -105,6 +145,43 @@ public class FilterChainTest {
 	}
 	
 	@Test
+	public void testNullResponseInterruptsFilterChain() throws InterruptedException {
+		
+		def queue = new LinkedList()
+		def filter1 = [handleCommandResponse:{response, chain -> queue.offer("filter1"); return response}] as MessageFilter
+		def filter2 = [handleCommandResponse:{response, chain -> null}] as MessageFilter
+		def filter3 = [handleCommandResponse:{response, chain -> queue.offer("filter3"); return response}] as MessageFilter
+		filtersChain.addFilter(filter1)
+		filtersChain.addFilter(filter2)
+		filtersChain.addFilter(filter3)
+		
+		def commandResponse = new Object()
+		filtersChain.handleCommandResponse(commandResponse)
+		assertEquals queue.poll(),"filter1"
+		assertNull queue.poll()
+	}
+	
+	@Test
+	public void testProtocolExceptionInResponseInterruptsFilterChain() throws InterruptedException {
+		
+		def queue = new LinkedList()
+		def filter1 = [handleCommandResponse:{response, chain -> queue.offer("filter1"); return response}] as MessageFilter
+		def filter2 = [handleCommandResponse:{response, chain -> throw new RayoProtocolException(Condition.BAD_REQUEST,Type.CANCEL,"error")}] as MessageFilter
+		def filter3 = [handleCommandResponse:{response, chain -> queue.offer("filter3"); return response}] as MessageFilter
+		filtersChain.addFilter(filter1)
+		filtersChain.addFilter(filter2)
+		filtersChain.addFilter(filter3)
+		
+		def commandResponse = new Object()
+		try {
+			filtersChain.handleCommandResponse(commandResponse)
+			fail("Expected Exception")
+		} catch (RayoProtocolException e) {}
+		assertEquals queue.poll(),"filter1"
+		assertNull queue.poll()
+	}
+	
+	@Test
 	public void testEventExecutedInSequence() throws InterruptedException {
 		
 		def queue = new LinkedList()
@@ -120,6 +197,43 @@ public class FilterChainTest {
 		assertEquals queue.poll(),"filter1"
 		assertEquals queue.poll(),"filter2"
 		assertEquals queue.poll(),"filter3"
+	}
+	
+	@Test
+	public void testNullEventInterruptsFilterChain() throws InterruptedException {
+		
+		def queue = new LinkedList()
+		def filter1 = [handleEvent:{event, chain -> queue.offer("filter1");return event}] as MessageFilter
+		def filter2 = [handleEvent:{event, chain -> null}] as MessageFilter
+		def filter3 = [handleEvent:{event, chain -> queue.offer("filter3");return event}] as MessageFilter
+		filtersChain.addFilter(filter1)
+		filtersChain.addFilter(filter2)
+		filtersChain.addFilter(filter3)
+		
+		def CallEvent event = [:] as CallEvent
+		filtersChain.handleEvent(event)
+		assertEquals queue.poll(),"filter1"
+		assertNull queue.poll()
+	}
+	
+	@Test
+	public void testProtocolExceptionInEventInterruptsFilterChain() throws InterruptedException {
+		
+		def queue = new LinkedList()
+		def filter1 = [handleEvent:{event, chain -> queue.offer("filter1");return event}] as MessageFilter
+		def filter2 = [handleEvent:{event, chain -> throw new RayoProtocolException(Condition.BAD_REQUEST,Type.CANCEL,"error")}] as MessageFilter
+		def filter3 = [handleEvent:{event, chain -> queue.offer("filter3");return event}] as MessageFilter
+		filtersChain.addFilter(filter1)
+		filtersChain.addFilter(filter2)
+		filtersChain.addFilter(filter3)
+		
+		def CallEvent event = [:] as CallEvent
+		try {
+			filtersChain.handleEvent(event)
+			fail("Expected Exception")
+		} catch (RayoProtocolException e) {}
+		assertEquals queue.poll(),"filter1"
+		assertNull queue.poll()
 	}
 	
 	@Test
