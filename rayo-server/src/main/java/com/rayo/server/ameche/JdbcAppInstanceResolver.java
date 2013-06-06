76d4d9c0fde43846cfb426fee4684b4a7bbc0c34
Michael Cervantes <mcervantes@voxeolabs.com>
2013-06-06 14:30:11 -0400

    Impelement jdbc app instance resolver [RAYO-112]

---
+++ rayo-server/src/main/java/com/rayo/server/ameche/JdbcAppInstanceResolver.java
@@ -0,0 +1,63 @@
+package com.rayo.server.ameche;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.dom4j.Element;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.rayo.core.CallDirection;
+import com.voxeo.logging.Loggerf;
+
+/**
+ * <p>Resolves Ameche routing rules defined on a external file, typically WEB-INF/ameche-routing.properties. 
+ * Routing rules will have into consideration both to/from fields. All instances matching the given offer will 
+ * be dispatched.</p>
+ * 
+ * @author martin
+ *
+ */
+public class JdbcAppInstanceResolver implements AppInstanceResolver {
+
+	private static final Loggerf logger = Loggerf.getLogger(JdbcAppInstanceResolver.class);
+
+	private String lookupSql;
+	private JdbcOperations jdbc;
+	
+	private RowMapper<AppInstance> rowMapper = new RowMapper<AppInstance>() {
+		@Override
+		public AppInstance mapRow(ResultSet rs, int rowNum)
+				throws SQLException {
+			String id = rs.getString("appInstanceId");
+			String uri = rs.getString("url");
+			try {
+				return new AppInstance(id, new URI(uri));
+			} catch (URISyntaxException ex) {
+				throw new IllegalStateException("Bad uri in database: " + uri.toString(), ex);
+			}
+		} 
+	};
+	
+	
+    @Override
+    public List<AppInstance> lookup(Element offer, CallDirection direction) {
+    	String from = offer.attributeValue("from");
+    	String to = offer.attributeValue("to");
+    	
+    	logger.debug("Finding a match for [from:%s,  to:%s, direction:%s]", from, to, direction);
+    	String address = ((direction == CallDirection.OUT) ? from : to);
+    	return jdbc.query(lookupSql, new Object[] { address }, rowMapper);
+    }
+    
+    public void setJdbcTemplate(JdbcOperations jdbc) {
+    	this.jdbc = jdbc;
+    }
+    
+    public void setLookupSql(String sql) {
+    	this.lookupSql = sql;
+    }
+}
---
+++ rayo-server/src/test/groovy/com/rayo/server/ameche/JdbcAppInstanceResolverTest.groovy
@@ -0,0 +1,73 @@
+package com.rayo.server.ameche
+
+import org.junit.Before;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.rayo.core.CallDirection;
+import com.tropo.test.AwesomeTestRunner;
+import com.tropo.test.Database;
+import com.tropo.test.MockResultSet;
+import com.tropo.test.MockResultSetMetaData;
+
+import org.dom4j.DocumentHelper;
+import org.gmock.*
+
+import static com.tropo.test.Assert.*
+import static com.tropo.test.Matchers.*
+import static org.gmock.GMock.*
+
+class JdbcAppInstanceResolverTest {
+
+	def gmc
+	def subject
+	def sql = 'foo'
+	def jdbc
+	
+	@Before
+	void init() {
+		gmc = new GMockController()
+		subject = new JdbcAppInstanceResolver()
+		jdbc = subject.jdbcTemplate = gmc.mock(JdbcOperations)
+		subject.lookupSql = sql
+	}
+	
+	@Test
+	void notNull() {
+		assertThat subject,is(notNullValue())
+	}
+	
+	@Test
+	void mapper() {
+		def addy = 'foobar'
+		def args = [addy] as Object[]
+		def columns = ['appInstanceId', 'url'] as String[]
+		def rows = [
+			[42, 'http://foo.bar:9999'] as Object[],
+			[45, 'http://foo.bar:9998'] as Object[],
+		] as Object[][]		
+		def rs = MockResultSet.create(MockResultSetMetaData.create(columns), rows)
+		jdbc.query(sql, args, match {RowMapper mapper->
+			(1..rows.length).each {rowIdx->
+				rs.next()
+				AppInstance instance = mapper.mapRow(rs, rowIdx)
+				println instance
+				assertThat instance,is(notNullValue())
+				assertThat instance.id,is(rows[rowIdx - 1][0].toString())				
+				assertThat instance.endpoint,is(URI.create(rows[rowIdx - 1][1]))				
+			} 
+			assertThat rs.next(),is(false)
+		})
+		gmc.play {
+			def offer = toXML("""<offer to="$addy" from="tel:+15613504458"/>""")
+			subject.lookup(offer, CallDirection.IN)
+		}
+	}
+	
+	private static toXML(s) {
+		DocumentHelper.parseText(s).rootElement
+	}
+}
