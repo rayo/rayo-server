f9cf64a30b581c7f40f2dac1c58c113b1996e022
mpermar <mpermar@voxeolabs.net>
2013-07-12 06:30:58 +0200

    AMECHE-226 : Enrich JDBC app instance lookup with subscription permissions

--- rayo-server/src/test/groovy/com/rayo/server/ameche/JdbcAppInstanceResolverTest.groovy
+++ rayo-server/src/test/groovy/com/rayo/server/ameche/JdbcAppInstanceResolverTest.groovy
@@ -44,10 +44,10 @@ class JdbcAppInstanceResolverTest {
 	void mapper() {
 		def addy = 'foobar'
 		def args = [addy] as Object[]
-		def columns = ['appInstanceId', 'url'] as String[]
+		def columns = ['appInstanceId', 'url', 'priority', 'permissions'] as String[]
 		def rows = [
-			[42, 'http://foo.bar:9999'] as Object[],
-			[45, 'http://foo.bar:9998'] as Object[],
+			[42, 'http://foo.bar:9999', 10, 4] as Object[],
+			[45, 'http://foo.bar:9998', 10, 4] as Object[],
 		] as Object[][]		
 		def rs = MockResultSet.create(MockResultSetMetaData.create(columns), rows)
 		jdbc.query(sql, args, match {RowMapper mapper->
@@ -57,7 +57,9 @@ class JdbcAppInstanceResolverTest {
 				println instance
 				assertThat instance,is(notNullValue())
 				assertThat instance.id,is(rows[rowIdx - 1][0].toString())				
-				assertThat instance.endpoint,is(URI.create(rows[rowIdx - 1][1]))				
+				assertThat instance.endpoint,is(URI.create(rows[rowIdx - 1][1]))
+				assertThat instance.priority, is(10)
+				assertThat instance.permissions, is(4)				
 			} 
 			assertThat rs.next(),is(false)
 			return true
