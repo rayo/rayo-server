f268dee0a44918bdd7ae20ca366a49d896f0bc84
Christoph Bussler <cbussler@voxeolabs.com>
2013-09-03 09:31:17 -0700

    Added test cases [AMECHE-327]

--- rayo-server/src/main/java/com/rayo/server/ameche/AppInstanceResolverS.java
+++ rayo-server/src/main/java/com/rayo/server/ameche/AppInstanceResolverS.java
@@ -12,6 +12,22 @@ public class AppInstanceResolverS {
 
 		logger.info("Original Address: " + address);
 
+		// Examples:
+		// sip:+12152065077@104.65.174.101;user=phone
+		// is looked up as...
+		// sip:+12152065077@104.65.174.101
+		// tel:+12152065077;sescase=term;regstate=reg
+		// is looked up as...
+		// tel:+12152065077
+
+		// parameter start after the first ';' and need to be
+		// dropped, including the ';'
+
+		Integer indexOfSemiColon = address.indexOf(";");
+		if (indexOfSemiColon >= 0) {
+			normalizedAddress = address.substring(0, indexOfSemiColon);
+		}
+
 		logger.info("Normalized Address: " + normalizedAddress);
 
 		return normalizedAddress;
