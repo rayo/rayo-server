24ef4250dd3973c6532c9649fa95dc22925a80e1
mpermar <mpermar@voxeolabs.net>
2013-09-06 18:46:00 +0200

    - AMECHE-296 : Make CDR disk location configurable in Chef

--- rayo-server/src/main/java/com/rayo/server/cdr/FileCdrStorageStrategy.java
+++ rayo-server/src/main/java/com/rayo/server/cdr/FileCdrStorageStrategy.java
@@ -5,24 +5,31 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.jmx.export.annotation.ManagedResource;
 
-import com.rayo.server.jmx.FileCdrMXBean;
 import com.rayo.core.cdr.Cdr;
 import com.rayo.core.cdr.CdrException;
+import com.rayo.server.jmx.FileCdrMXBean;
 import com.voxeo.logging.Loggerf;
 
 @ManagedResource(objectName = "com.rayo:Type=Admin,name=File CDR", description = "Filebased CDR storage")
 public class FileCdrStorageStrategy implements CdrStorageStrategy, FileCdrMXBean {
 
 	private Loggerf logger = Loggerf.getLogger(FileCdrStorageStrategy.class);
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 	
+	// will keep this one for legacy reasons
 	private String path;
+	private String baseFolder = "/tmp/cdrs";
+	
 	private boolean append = true;
+	private String lastUsedPath;
 	
 	private OutputStream out;
 
@@ -33,20 +40,31 @@ public class FileCdrStorageStrategy implements CdrStorageStrategy, FileCdrMXBean
 	
 		logger.info("Initializing Filebased CDR storage");
 		
-		if (path == null) {
-			throw new IllegalStateException("Don't know where to write CDRs. You need to set the path variable.");
+		if (baseFolder == null && path == null) {
+			throw new IllegalStateException("Don't know where to write CDRs. You need to set the baseFolder variable.");
 		}
 		
-		File file = new File(path);
-		if (!file.exists()) {
-			File parent = file.getParentFile();
-			if (!parent.exists()) {
-				parent.mkdirs();
+		if (baseFolder != null) {
+			File f = new File(baseFolder);
+			if (!f.exists()) {
+				boolean result = f.mkdirs();
+				if (!result) {
+					throw new IllegalStateException(String.format("Could not create CDRs folder %s", f.getAbsolutePath()));
+				}
 			}
-		}
-		
-		out = new BufferedOutputStream(new FileOutputStream(file,append));
-		
+			// unset legacy stuff
+			path = null;
+		} else {
+			File file = new File(path);
+			if (!file.exists()) {
+				File parent = file.getParentFile();
+				if (!parent.exists()) {
+					parent.mkdirs();
+				}
+			}
+			out = new BufferedOutputStream(new FileOutputStream(file,append));
+		}	
+		lastUsedPath = null;
 	}
 	
 	public void shutdown() {
@@ -65,6 +83,9 @@ public class FileCdrStorageStrategy implements CdrStorageStrategy, FileCdrMXBean
 	public void store(Cdr cdr) throws CdrException {
 		
 		try {
+			if (baseFolder != null) {
+				out = getFileStream(cdr);
+			}
 			lock.readLock().lock();
 			out.write(cdr.toString().getBytes());
 			out.flush();
@@ -75,6 +96,33 @@ public class FileCdrStorageStrategy implements CdrStorageStrategy, FileCdrMXBean
 		}
 	}
 	
+	private synchronized OutputStream getFileStream(Cdr cdr) {
+
+		OutputStream oldOut = out;
+		path = sdf.format(new Date(cdr.getStartTime()));
+		if (!path.equals(lastUsedPath)) {
+			File f = new File(baseFolder + "/" + path + ".xml");
+			if (!f.exists()) {
+				if (out != null) {
+					try {
+						out.close();
+					} catch (IOException ioe) {
+						logger.error(ioe.getMessage(), ioe);
+					}
+				}
+				try {
+					out = new BufferedOutputStream(new FileOutputStream(f));
+				} catch (IOException ioe) {
+					logger.error(ioe.getMessage(), ioe);
+					out = oldOut;
+				}
+			}
+		} 
+		lastUsedPath = path;
+		return out;
+		
+	}
+
 	@Override
 	@ManagedOperation(description = "Change CDRs audit file")
 	public void changeFile(String filename) {
@@ -106,5 +154,17 @@ public class FileCdrStorageStrategy implements CdrStorageStrategy, FileCdrMXBean
 
 	public void setAppend(boolean append) {
 		this.append = append;
+	}
+
+	public String getBaseFolder() {
+		return baseFolder;
+	}
+
+	public void setBaseFolder(String baseFolder) {
+		
+		if (baseFolder.endsWith("/")) {
+			baseFolder = baseFolder.substring(0, baseFolder.length()-1);
+		}
+		this.baseFolder = baseFolder;
 	}
 }
---
+++ rayo-server/src/test/java/com/rayo/server/cdr/FileCdrStorageStrategyTest.java
@@ -0,0 +1,110 @@
+package com.rayo.server.cdr;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.lang.math.RandomUtils;
+import org.apache.commons.lang.time.DateUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.rayo.core.cdr.Cdr;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/rayo-context-activemq-cdr.xml")
+public class FileCdrStorageStrategyTest {
+
+	@Autowired
+	FileCdrStorageStrategy storage;
+	
+	@Test
+	public void testFolderIsCreatedOnInit() throws Exception {
+		
+		storage.setBaseFolder("target/test" + RandomUtils.nextInt(10000));
+		File folder = new File(storage.getBaseFolder());
+		folder.deleteOnExit();
+		assertFalse(folder.exists());
+		storage.init();
+		assertTrue(folder.exists());
+	}
+	
+	
+	@Test
+	public void testCdrFileIsCreated() throws Exception {
+		
+		storage.setBaseFolder("target/test" + RandomUtils.nextInt(10000));
+		File folder = new File(storage.getBaseFolder());
+		folder.deleteOnExit();
+		storage.init();
+		assertEquals(folder.listFiles().length, 0);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date now = new Date();
+		Cdr cdr = new Cdr();
+		cdr.setStartTime(now.getTime());
+		storage.store(cdr);
+
+		String expectedFile = sdf.format(now) + ".xml"; 
+		assertEquals(folder.listFiles().length, 1);
+		assertTrue(folder.listFiles()[0].getName().endsWith(expectedFile));
+	}
+	
+	
+	@Test
+	public void testCdrFilesRollOver() throws Exception {
+		
+		storage.setBaseFolder("target/test" + RandomUtils.nextInt(10000));
+		File folder = new File(storage.getBaseFolder());
+		folder.deleteOnExit();
+		assertFalse(folder.exists());
+		storage.init();
+		assertEquals(folder.listFiles().length, 0);		
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date now = new Date();
+		Cdr cdr = new Cdr();
+		cdr.setStartTime(now.getTime());
+		storage.store(cdr);
+		String expectedFile = sdf.format(now) + ".xml"; 
+		assertEquals(folder.listFiles().length, 1);
+		assertTrue(folder.listFiles()[0].getName().endsWith(expectedFile));
+
+		Date tomorrow = DateUtils.addDays(now, 1);
+		cdr = new Cdr();
+		cdr.setStartTime(tomorrow.getTime());
+		storage.store(cdr);
+		expectedFile = sdf.format(tomorrow) + ".xml"; 
+		assertEquals(folder.listFiles().length, 2);
+		assertTrue(folder.listFiles()[1].getName().endsWith(expectedFile));
+	}
+	
+	@Test
+	public void testAppendCdrs() throws Exception {
+		
+		storage.setBaseFolder("target/test" + RandomUtils.nextInt(10000));
+		File folder = new File(storage.getBaseFolder());
+		folder.deleteOnExit();
+		storage.init();
+		
+		Date now = new Date();
+		Cdr cdr = new Cdr();
+		cdr.setStartTime(now.getTime());
+		storage.store(cdr);
+		assertEquals(folder.listFiles().length, 1);
+		long length = folder.listFiles()[0].length();
+
+		cdr = new Cdr();
+		cdr.setStartTime(now.getTime());
+		storage.store(cdr);
+		assertEquals(folder.listFiles().length, 1);
+		assertTrue(folder.listFiles()[0].length() > length);
+	}
+}
