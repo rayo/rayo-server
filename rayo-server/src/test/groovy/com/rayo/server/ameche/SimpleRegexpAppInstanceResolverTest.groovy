b914c4b71a1e935715c967235fd510963c91f35f
Christoph Bussler <cbussler@voxeolabs.com>
2013-09-20 09:58:39 -0700

    Adding test case [AMECHE-327]

--- rayo-server/src/test/groovy/com/rayo/server/ameche/SimpleRegexpAppInstanceResolverTest.groovy
+++ rayo-server/src/test/groovy/com/rayo/server/ameche/SimpleRegexpAppInstanceResolverTest.groovy
@@ -98,4 +98,26 @@ class SimpleRegexpAppInstanceResolverTest {
 				"http://127.0.0.1:4444"));
 		assertTrue(instances.get(0).isRequired());
 	}
+
+	@Test
+	void resolveRequiredAppInstanceWPServedUser() throws Exception {
+
+		Resource routes = new ByteArrayResource(
+				".*=1:http://127.0.0.1:4444:true".getBytes());
+		SimpleRegexpAppInstanceResolver resolver = new SimpleRegexpAppInstanceResolver(
+				routes);
+		resolver.setCallManager(callManager);
+
+		Element offerElement = DocumentHelper.parseText(
+				"<offer to=\"tel:+13055195825;biz=baz\" from=\"tel:+15613504458;bling=blang\"> <header name=\"P-Served-User\" value=\"tel:+12152065077;sescase=term;regstate=reg\"/></offer>")
+				.getRootElement();
+		List<AppInstance> instances = resolver.lookup(offerElement,
+				CallDirection.IN);
+
+		assertNotNull(instances);
+		assertEquals(instances.size(), 1);
+		assertEquals(instances.get(0).getEndpoint(), new URI(
+				"http://127.0.0.1:4444"));
+		assertTrue(instances.get(0).isRequired());
+	}
 }
