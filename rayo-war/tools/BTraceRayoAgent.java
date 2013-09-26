2b802fd9296acdc5ae5c458bec24051450083cca
mpermar <mpermar@voxeolabs.net>
2013-09-26 10:26:01 +0200

    - AMECHE-355 : Exhaustive list of Moho and JSR 309 Dependencies
    Added a BTrace agent to the WAR build. Note it will not be used unless
    the -javaagent line is added to Prism.

---
+++ rayo-war/tools/BTraceRayoAgent.java
@@ -0,0 +1,34 @@
+import com.sun.btrace.annotations.*;
+import static com.sun.btrace.BTraceUtils.*;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@BTrace
+public class BTraceRayoAgent {
+
+        private static Map<String, AtomicInteger> histo = Collections.newHashMap();
+
+        @OnMethod(
+                clazz="/javax\\.media\\..*/",
+                method="/.*/"
+        )
+        public static void m(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) {
+
+                String key = strcat(probeClass, strcat(".",probeMethod));
+                AtomicInteger ai = Collections.get(histo, key);
+                if (ai == null) {
+                        ai = Atomic.newAtomicInteger(1);
+                        Collections.put(histo, key, ai);
+                } else {
+                        Atomic.incrementAndGet(ai);
+                }
+        }
+
+        @OnTimer(4000)
+        public static void print() {
+
+                if (Collections.size(histo) != 0) {
+                        printNumberMap("Component Histogram", histo);
+                }
+        }
+}
